07e4f39aaca4c31853b62696246e8bbb818b5c31
Stas Kelvich <stas.kelvich@gmail.com>
2023-03-08 23:37:19 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,53 @@
+# pg_tiktoken
+
+Postgres extension that does input tokenization using OpenAI's tiktoken.
+
+## Usage
+
+```sql
+pg_tiktoken=# create extension pg_tiktoken;
+CREATE EXTENSION
+pg_tiktoken=# select tiktoken_count('p50k_edit', 'A long time ago in a galaxy far, far away');
+ tiktoken_count 
+----------------
+             11
+(1 row)
+
+pg_tiktoken=# select tiktoken_encode('cl100k_base', 'A long time ago in a galaxy far, far away');
+                  tiktoken_encode                   
+----------------------------------------------------
+ {32,1317,892,4227,304,264,34261,3117,11,3117,3201}
+(1 row)
+```
+
+## Supported models
+
+
+| Encoding name           | OpenAI models                                       |
+|-------------------------|-----------------------------------------------------|
+| `cl100k_base`           | ChatGPT models, `text-embedding-ada-002`            |
+| `p50k_base`             | Code models, `text-davinci-002`, `text-davinci-003` |
+| `p50k_edit`             | Use for edit models like `text-davinci-edit-001`, `code-davinci-edit-001` |
+| `r50k_base` (or `gpt2`) | GPT-3 models like `davinci`                         |
+
+`tiktoken_count` and `tiktoken_encode` accept both encoding name and OpenAI model name as a first argument.
+
+
+## Installation
+
+Assuming that rust toolchain is already istalled:
+
+```
+# install pgx
+cargo install --locked cargo-pgx
+cargo pgx init
+# build and install pg_tiktoken
+git clone https://github.com/kelvich/pg_tiktoken
+cd pg_tiktoken
+cargo pgx install
+```
+
+## Kudos
+
+https://github.com/zurawiki/tiktoken-rs
+https://github.com/openai/tiktoken
---
+++ src/lib.rs
@@ -0,0 +1,116 @@
+use pgx::prelude::*;
+
+use tiktoken_rs::encoding_for_model;
+use tiktoken_rs::r50k_base;
+use tiktoken_rs::cl100k_base;
+use tiktoken_rs::p50k_base;
+use tiktoken_rs::p50k_edit;
+
+pgx::pg_module_magic!();
+
+// encode to the array of tokens using given encoding/model
+//
+// encoding_selector could be the name of the encoding
+// (cl100k_base, p50k_base, ...)
+// or designated model (text-davinci-003, gpt-3.5-turbo, ...)
+fn encode_with_model(encoding_selector: &str, text: &str) -> Vec<usize> {
+    let encoder_name = encoding_for_model(encoding_selector)
+        .unwrap_or(encoding_selector);
+
+    let encoder = match encoder_name {
+        "cl100k_base" => cl100k_base(),
+        "r50k_base" | "gpt2" => r50k_base(),
+        "p50k_base" => p50k_base(),
+        "p50k_edit" => p50k_edit(),
+        _ => error!("'{encoding_selector}': unknown model or encoder")
+    }.unwrap();
+
+    encoder.encode_with_special_tokens(text)
+}
+
+#[pg_extern]
+fn tiktoken_encode(encoding_selector: &str, text: &str) -> Vec<i64> {
+    encode_with_model(encoding_selector, text)
+        .iter()
+        .map(|&x| x.try_into().unwrap())
+        .collect()
+}
+
+#[pg_extern]
+fn tiktoken_count(encoding_selector: &str, text: &str) -> i64 {
+    encode_with_model(encoding_selector, text)
+        .len()
+        .try_into()
+        .unwrap()
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pg_schema]
+mod tests {
+    use pgx::prelude::*;
+
+    #[pg_test]
+    fn test_tiktoken_encode() {
+        // check various encoders
+        let str = "This is a test         with a lot of spaces<|endoftext|>";
+        assert_eq!(
+            crate::tiktoken_encode("p50k_base", str),
+            vec![1212, 318, 257, 1332, 50263, 351, 257, 1256, 286, 9029, 50256]
+        );
+        assert_eq!(
+            crate::tiktoken_encode("p50k_edit", str),
+            vec![1212, 318, 257, 1332, 50263, 351, 257, 1256, 286, 9029, 50256]
+        );
+        assert_eq!(
+            crate::tiktoken_encode("r50k_base", str),
+            vec![1212, 318, 257, 1332, 220, 220, 220, 220, 220, 220, 220, 220, 351, 257, 1256, 286, 9029, 50256]
+        );
+        assert_eq!(
+            crate::tiktoken_encode("cl100k_base", str),
+            vec![2028, 374, 264, 1296, 260, 449, 264, 2763, 315, 12908, 100257]
+        );
+
+        // check model aliases
+        let str = "A long time ago in a galaxy far, far away";
+        assert_eq!(
+            crate::tiktoken_encode("text-davinci-002", str),
+            crate::tiktoken_encode("p50k_base", str)
+        );
+        assert_eq!(
+            crate::tiktoken_encode("gpt-3.5-turbo", str),
+            crate::tiktoken_encode("cl100k_base", str)
+        );
+        assert_eq!(
+            crate::tiktoken_encode("gpt2", str),
+            crate::tiktoken_encode("r50k_base", str)
+        );
+        assert_eq!(
+            crate::tiktoken_encode("code-davinci-edit-001", str),
+            crate::tiktoken_encode("p50k_edit", str)
+        );
+    }
+
+    #[pg_test]
+    fn test_tiktoken_count() {
+        let str = "A long time ago in a galaxy far, far away";
+        assert_eq!(crate::tiktoken_count("p50k_base", str), 11);
+        assert_eq!(crate::tiktoken_count("cl100k_base", str), 11);
+        assert_eq!(crate::tiktoken_count("r50k_base", str), 11);
+        assert_eq!(crate::tiktoken_count("p50k_edit", str), 11);
+    }
+
+}
+
+/// This module is required by `cargo pgx test` invocations. 
+/// It must be visible at the root of your extension crate.
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
+    }
+}
